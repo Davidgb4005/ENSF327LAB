2739e89dffef6e5f0c1bba6ace270bd6abb88179
Davidgb4005 <124512857+Davidgb4005@users.noreply.github.com>
2024-06-10 12:50:20 -0600

    Create transpose.cpp

---
+++ Lab5A/transpose.cpp
@@ -0,0 +1,69 @@
+// ENSF 337 - Lab 5  - exercise A
+// lab5ExA.cpp
+
+#include<vector>
+#include<string>
+#include <iostream>
+using std::cout;
+using std::cerr;
+using std::endl;
+using std::vector;
+using std::string;
+
+typedef vector<string> String_Vector;
+
+String_Vector transpose(const String_Vector& sv);
+// REQUIRES:
+//    sv.size() >= 1
+//    All the strings in sv are the same length, and that length is >= 1.
+// PROMISES:
+//    Return value is the "transpose" of sv, as defined in the Exercise B
+//    instructions.
+
+int main() {
+    
+    const int ROWS = 5;
+    const int COLS = 4;
+    
+    char c = 'A';
+    String_Vector sv;
+    sv.resize(ROWS);
+    for(int i = 0; i < ROWS; i++)
+        for(int j = 0; j < COLS; j++) {
+            sv.at(i).push_back(c);
+            c++;
+            if(c == 'Z' + 1)
+                c = 'a';
+            else if (c == 'z' + 1)
+                c = 'A';
+        }
+    
+    
+    for(int i = 0; i < ROWS; i++) {
+        cout<< sv.at(i);
+        cout << endl;
+    }
+    
+    String_Vector vs = transpose(sv);
+    for(int i = 0; i < (int)vs.size(); i++)
+        cout << vs.at(i) << endl;
+    
+    return 0;
+}
+
+
+
+String_Vector transpose (const String_Vector& sv) {
+    
+  int vs_cols = sv.size();
+  int vs_rows = sv[0].size();
+  String_Vector vs;
+  vs.resize(vs_rows);
+  for (int i =0; i<vs_rows;i++){
+    for(int j = 0; j < vs_cols; j++){
+      vs.at(i).push_back(sv[j][i]);
+    }
+  }
+   return vs;
+    
+}
